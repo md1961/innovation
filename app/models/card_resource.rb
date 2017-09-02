7c9e06271140d30e169b5918a6b997c8e1c9a303
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2017-09-02 10:41:26 +0900

    Rename belongs_to :resource_position of CardResource to :position

--- app/models/card_resource.rb
+++ app/models/card_resource.rb
@@ -1,5 +1,5 @@
 class CardResource < ActiveRecord::Base
   belongs_to :card
   belongs_to :resource
-  belongs_to :resource_position
+  belongs_to :position, class_name: 'ResourcePosition', foreign_key: 'resource_position_id'
 end
