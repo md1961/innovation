55b23e0bb7533175f4eddaba6bad2a7cbc91e4a2
Naoyuki Kumagai <kumagai@mimas>
2017-09-05 08:59:32 +0900

    Equip with functionality to turn on/off color display from URL query string

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,17 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :set_color
+
+  protected
+
+    def set_color
+      case params[:color]
+      when 'true'
+        session[:uses_color] = true
+      when 'false'
+        session[:uses_color] = false
+      end
+      @uses_color = session[:uses_color]
+    end
 end
--- app/helpers/cards_helper.rb
+++ app/helpers/cards_helper.rb
@@ -1,9 +1,14 @@
 module CardsHelper
 
+  def color_class(model)
+    return '' unless @uses_color && model
+    model.color.name_eng
+  end
+
   def card_effect_in_span(effect)
     content_tag :span do
       concat (
-        content_tag :span, effect.resource, class: effect.resource.color.name_eng
+        content_tag :span, effect.resource, class: color_class(effect.resource)
       )
       concat "#{effect.is_for_all ? '+' : '>'} #{effect.content}"
     end
@@ -14,10 +19,10 @@ module CardsHelper
   end
 
   def card_resource_in_div(card, pos)
-    value = card.resource_at(pos) || card.age.level
-    color = value.is_a?(Integer) ? 'age' : value.color.name_eng
+    model = card.resource_at(pos) || card.age.level
+    color = model.is_a?(Integer) ? 'age' : color_class(model)
     content_tag :div, class: 'card_resource' do
-      content_tag :span, value, class: color
+      content_tag :span, model, class: color
     end
   end
 end
