9757b17debd5f09ec1973c1958ca7b4d7b012ad7
Naoyuki Kumagai <kumagai@mimas>
2017-09-06 08:20:51 +0900

    Modify Game to destroy playings and stocks as well on destroy

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,7 +1,7 @@
 class Game < ActiveRecord::Base
-  has_many :playings, -> { order(:ordering) }
+  has_many :playings, -> { order(:ordering) }, dependent: :destroy
   has_many :players, through: :playings
-  has_many :stocks, -> { order(:age_id) }
+  has_many :stocks, -> { order(:age_id) }, dependent: :destroy
   belongs_to :current_player, class_name: 'Player', foreign_key: :current_player_id
 
   after_create :prepare
