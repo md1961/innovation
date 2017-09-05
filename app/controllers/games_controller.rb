27c4ffd1cb47a8e5c08dfe9a2af122db2b832fd5
Naoyuki Kumagai <kumagai@mimas>
2017-09-05 15:20:31 +0900

    'games#index' to prepare a Game with current_player and Playing's orderings

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,10 +2,15 @@ class GamesController < ApplicationController
 
   def index
     if Game.count.zero?
-      Game.create!(num_players: 2).tap { |game|
-        game.players.create!(name: 'Hmn', is_computer: false)
-        game.players.create!(name: 'Cmp', is_computer: true )
-      }
+      Game.transaction do
+        Game.create!(num_players: 2).tap { |game|
+          player0 = Player.create!(name: 'Hmn', is_computer: false)
+          player1 = Player.create!(name: 'Cmp', is_computer: true )
+          game.playings.create!(player: player0, ordering: 0)
+          game.playings.create!(player: player1, ordering: 1)
+          game.update!(current_player: player0)
+        }
+      end
     end
     redirect_to Game.order(:created_at).last
   end
--- app/models/game.rb
+++ app/models/game.rb
@@ -2,6 +2,7 @@ class Game < ActiveRecord::Base
   has_many :playings
   has_many :players, through: :playings
   has_many :stocks, -> { order(:age_id) }
+  belongs_to :current_player, class_name: 'Player', foreign_key: :current_player_id
 
   after_create :prepare
 
