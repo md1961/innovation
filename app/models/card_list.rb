0ee03b0a72861561a787a5fa982db676f12665c0
Naoyuki Kumagai <kumagai@mimas>
2017-09-06 16:38:47 +0900

    Add dependent: :destroy to has_many :card_list_items

--- app/models/card_list.rb
+++ app/models/card_list.rb
@@ -2,7 +2,7 @@ class CardList < ActiveRecord::Base
   belongs_to :game
   belongs_to :player
   belongs_to :color
-  has_many :card_list_items, -> { order(:ordering) }
+  has_many :card_list_items, -> { order(:ordering) }, dependent: :destroy
   has_many :cards, through: :card_list_items
 
   def add(card)
