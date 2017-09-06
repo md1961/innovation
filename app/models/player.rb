54771a149b51e925fd97761fea3c228d2bda850c
Naoyuki Kumagai <kumagai@mimas>
2017-09-06 09:52:35 +0900

    Add has_many :boards and #boards_for() to Player

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,9 +1,14 @@
 class Player < ActiveRecord::Base
   has_many :playings
   has_many :hands
+  has_many :boards, -> { order(:color_id) }
 
   def hand_for(game)
     hands.find_by(game: game)
+  end
+
+  def boards_for(game)
+    boards.where(game: game)
   end
 
   def draw_from(stock)
