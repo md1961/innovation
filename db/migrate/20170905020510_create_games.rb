4a53f88b43d442dda04d8aaee15ffe6a6e771d0d
Naoyuki Kumagai <kumagai@mimas>
2017-09-08 10:01:18 +0900

    Add attr turn_player_id to Game

--- db/migrate/20170905020510_create_games.rb
+++ db/migrate/20170905020510_create_games.rb
@@ -3,8 +3,9 @@ class CreateGames < ActiveRecord::Migration
   def change
     create_table :games do |t|
       t.integer :num_players      , null: false, default: 2
-      t.integer :current_player_id
+      t.integer :turn_player_id
       t.integer :num_actions_left , null: false, default: 1
+      t.integer :current_player_id
       t.string  :remark
 
       t.timestamps null: false
