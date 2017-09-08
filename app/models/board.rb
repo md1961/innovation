5a68647443c8d8fad186c3d95fdbb9ec3563838a
Naoyuki Kumagai <kumagai@mimas>
2017-09-08 11:51:34 +0900

    Add Board#resource_counts()

--- app/models/board.rb
+++ app/models/board.rb
@@ -3,6 +3,19 @@ class Board < CardList
 
   def resource_counts
     return {} if cards.empty?
-    cards.last.resources.inject(Hash.new(0)) { |h, r| h[r] += 1; h }
+
+    card_side = \
+      if expanded_left?
+        Card::RIGHT
+      elsif expanded_right?
+        Card::LEFT
+      elsif expanded_upward?
+        Card::BOTTOM
+      else
+        nil
+      end
+    resources_expanded = cards[0 .. -2].flat_map { |card| card.resources_at(card_side) }
+
+    (cards.last.resources + resources_expanded).inject(Hash.new(0)) { |h, r| h[r] += 1; h }
   end
 end
