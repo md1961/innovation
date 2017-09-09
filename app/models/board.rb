d0001e1b6a8971cce773acc9b2c5cbe230aa1f81
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2017-09-09 18:03:10 +0900

    Fix Board to set not_expanded if cards.size become less than two

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,6 +1,8 @@
 class Board < CardList
   enum expansion: {not_expanded: 0, expanded_left: 1, expanded_right: 2, expanded_upward: 3}
 
+  after_save :unexpand
+
   def resource_counts
     return {} if cards.empty?
 
@@ -18,4 +20,10 @@ class Board < CardList
 
     (cards.last.resources + resources_expanded).inject(Hash.new(0)) { |h, r| h[r] += 1; h }
   end
+
+  private
+
+    def unexpand
+      self.expansion = 0 if cards.size < 2
+    end
 end
