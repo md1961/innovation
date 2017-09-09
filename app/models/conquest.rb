dce978b9fb8e5b35129d6ab7856b3aebd0011a94
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2017-09-09 20:27:14 +0900

    Fix Conquest to allow nil when validating uniqueness of age and category

--- app/models/conquest.rb
+++ app/models/conquest.rb
@@ -4,8 +4,8 @@ class Conquest < ActiveRecord::Base
   belongs_to :age
   belongs_to :category
 
-  validates :age_id     , uniqueness: {scope: :game_id}
-  validates :category_id, uniqueness: {scope: :game_id}
+  validates :age_id     , uniqueness: {scope: :game_id}, allow_nil: true
+  validates :category_id, uniqueness: {scope: :game_id}, allow_nil: true
 
   def age?
     type == 'AgeConquest'
