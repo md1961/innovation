47b8256cef0a84ed02eba2bd011d83beb6233c78
Naoyuki Kumagai <kumagai@mimas>
2017-09-12 16:02:26 +0900

    Add GameEvaluator#players_with_more_resource_than()

--- app/models/game_evaluator.rb
+++ app/models/game_evaluator.rb
@@ -1,17 +1,21 @@
 class GameEvaluator
 
-  def initialize(game)
+  def initialize(game, player)
     @game = game
+    @player = player
   end
 
   def inexecutable?(board)
     return false unless board.active_card.forcing?
     resource = board.active_card.effects.first.resource
-    owner_player = board.player
-    @game.other_players_than(owner_player).any? { |player|
-      count       = player      .resource_counts(@game)[resource] 
-      owner_count = owner_player.resource_counts(@game)[resource]
-      count >= owner_count
+    players_with_more_resource_than(board.player, resource).size >= 1
+  end
+
+  def players_with_more_resource_than(this_player, resource)
+    this_count = this_player.resource_counts(@game)[resource]
+    @game.other_players_than(this_player).find_all { |player|
+      count = player.resource_counts(@game)[resource]
+      count >= this_count
     }
   end
 end
