eedae04ee4585535ae83c3b47c6a815fae0cf0f5
Naoyuki Kumagai <kumagai@mimas>
2017-09-14 13:56:13 +0900

    Add Conquest.targets() and .conquerable_targets()

--- app/models/conquest.rb
+++ app/models/conquest.rb
@@ -7,6 +7,15 @@ class Conquest < ActiveRecord::Base
   validates :age_id     , uniqueness: {scope: :game_id}, allow_nil: true
   validates :category_id, uniqueness: {scope: :game_id}, allow_nil: true
 
+
+  def self.targets
+    @targets ||= Age.all + Category.all
+  end
+
+  def self.conquerable_targets(player, game)
+    targets.find_all { |target| target.conquerable?(player, game) }
+  end
+
   def age?
     type == 'AgeConquest'
   end
