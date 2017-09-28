d0bcf21d2e5a31def3c5ef0b8b25d77b3aacd823
Naoyuki Kumagai <kumagai@mimas>
2017-09-28 13:38:34 +0900

    Modify action options display to emphasize depending on weight value

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -9,4 +9,22 @@ module GamesHelper
     player.conquests_for(@game).find_all(&:category?)
       .map(&:category).map(&:name).map(&:first).join(' ')
   end
+
+  def action_options_display(action_options)
+    return nil unless action_options
+    action_options.split(/\s+/).map { |option|
+      if option =~ /(\d+)\)\z/
+        weight = Integer(Regexp.last_match(1))
+        if weight >= 200
+          "<b>#{option}</b>"
+        elsif weight.zero?
+          "<strike>#{option}</strike>"
+        else
+          option
+        end
+      else
+        option
+      end
+    }.join(' ').html_safe
+  end
 end
