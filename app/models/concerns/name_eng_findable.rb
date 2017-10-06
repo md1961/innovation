36c1283c309a80ef3a328e699096faa5f84e2792
Naoyuki Kumagai <kumagai@mimas>
2017-10-06 15:05:35 +0900

    Modify NameEngFindable to use lazy initialization to speed up

--- app/models/concerns/name_eng_findable.rb
+++ app/models/concerns/name_eng_findable.rb
@@ -3,7 +3,9 @@ module NameEngFindable
   def self.extended(model)
     model.pluck(:name_eng).each do |name|
       model.define_singleton_method(name.downcase) do
-        model.find_by(name_eng: name)
+        var_name = "@#{name.downcase}"
+        model.instance_variable_get(var_name) || \
+          model.instance_variable_set(var_name, model.find_by(name_eng: name))
       end
     end
   end
