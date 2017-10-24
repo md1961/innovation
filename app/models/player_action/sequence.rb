ea998e871bb23f81397dead6ce8d2aaf422cabd0
Naoyuki Kumagai <kumagai@mimas>
2017-10-25 07:36:38 +0900

    Change PA::Sequence not to handle conditions nor branches

--- app/models/player_action/sequence.rb
+++ app/models/player_action/sequence.rb
@@ -2,39 +2,25 @@ module PlayerAction
 
 class Sequence
 
-  def initialize(game_evaluator, actions, condition = 'true')
-    @game_evaluator = game_evaluator
-    @condition = condition
+  def initialize(actions = [])
     @actions = actions
+    @undo_statements = []
   end
 
-  def set_branch(game_evaluator, condition, actions_true, actions_false)
-    @branch = Branch.new(game_evaluator, condition, actions_true, actions_false)
+  def add(action)
+    @actions << action
+  end
+
+  def undo_statement
+    @undo_statements.join('; ')
   end
 
   def perform
-    if @game_evaluator.boolean_eval(@condition)
-      @actions.map(&:perform)
-    end
-    @branch&.perform
-  end
-
-  class Branch
-
-    def initialize(game_evaluator, condition, actions_true, actions_false)
-      @game_evaluator = game_evaluator
-      @condition = condition
-      @actions_true = actions_true
-      @actions_false = actions_false
-    end
-
-    def perform
-      if @game_evaluator.boolean_eval(@condition)
-        @actions_true.map(&:perform)
-      else
-        @actions_false.map(&:perform)
-      end
-    end
+    @actions.flat_map { |action|
+      action.perform.tap { |action|
+        @undo_statements.unshift(action.undo_statement)
+      }
+    }
   end
 end
 
